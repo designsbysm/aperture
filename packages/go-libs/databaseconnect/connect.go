6c331231eb5a83e6c8c7a2d540235b82e08d4570
Scott A. Matthews <scott@designsbysm.com>
2022-11-27 10:02:45 -0700

    update(go-libs): refactor databaseconnent

--- packages/go-libs/databaseconnect/connect.go
+++ packages/go-libs/databaseconnect/connect.go
@@ -1,18 +1,27 @@
 package databaseconnect
 
 import (
-	"errors"
 	"fmt"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
-func Go(connection string, schemaName string, logLevel int) (db *gorm.DB, err error) {
+func Go(schemaName string) (db *gorm.DB, err error) {
+	connection := viper.GetString("URL_DATABASE")
 	if connection == "" {
-		return nil, errors.New("connection string is missing")
+		user := viper.GetString("DATABASE_USER")
+		password := viper.GetString("DATABASE_PASSWORD")
+		host := viper.GetString("URL_DOCKER")
+		port := viper.GetString("PORT_DATABASE")
+		name := viper.GetString("DATABASE_NAME")
+
+		connection = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, name)
 	}
+
+	logLevel := viper.GetInt("gorm.level")
 
 	return gorm.Open(
 		postgres.Open(connection),
--- packages/service-authentication/database/init.go
+++ packages/service-authentication/database/init.go
@@ -1,33 +1,14 @@
 package database
 
 import (
-	"fmt"
-
 	"github.com/smaperture/go-libs/databaseconnect"
-	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Init() (err error) {
-	connection := viper.GetString("URL_DATABASE")
-	if connection == "" {
-		user := viper.GetString("DATABASE_USER")
-		password := viper.GetString("DATABASE_PASSWORD")
-		host := viper.GetString("URL_DOCKER")
-		port := viper.GetString("PORT_DATABASE")
-		name := viper.GetString("DATABASE_NAME")
+	DB, err = databaseconnect.Go("authentication")
 
-		connection = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, name)
-	}
-
-	logLevel := viper.GetInt("gorm.level")
-
-	DB, err = databaseconnect.Go(connection, "authentication", logLevel)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
